f3681ea837969dbf70ad211a4c935b671dfcd8e5
bjmashele <bjmashele@gmail.com>
2026-06-19 20:44:15 -0500

    Create a vector (function: get_or_create_vector_store)

--- server.py
+++ server.py
@@ -14,3 +14,13 @@ client = OpenAI()
 VECTOR_STORE_NAME = "MEMORIESTORE"
 
 mcp = FastMCP('Memories')
+
+# Create or get the vector store for memories (this will be used to store and retrieve memories) 
+# [Reference OPENAI boilerplate code for  vector stores](https://platform.openai.com/docs/guides/vector-stores/quickstart)
+def get_or_create_vector_store():
+    # Try to find existing vector store, else create
+    stores = client.vector_stores.list()
+    for store in stores:
+        if store.name == VECTOR_STORE_NAME:
+            return store
+    return client.vector_stores.create(name=VECTOR_STORE_NAME)
